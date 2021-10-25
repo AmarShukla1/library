b28f13bde906253e807fd2e48473ab9ef20c6607
AmarShukla1 <spamfreemenow12345@gmail.com>
2021-10-25 17:54:32 +0530

    OOPS

--- logic.js
+++ logic.js
@@ -1,3 +1,16 @@
+//for simple things this is
+//although probably right.But typically the ui reflects the data not be the sole place where it is.
+//therefore will add an object oriented way.
+
+
+let mylibrary=[]
+
+
+function createbook(name,title,pages){
+ this.name=name;
+ this.pages=pages;
+ this.title=title;
+}
 const button=document.querySelector('button');
 const container=document.querySelector('.books-container');
 const close=document.querySelector('.close');
@@ -22,13 +35,16 @@ modal.classList.remove('show');
 const div=document.createElement('div');
     div.className='box';
     div.innerHTML=document.querySelector('.x').value+'<br>'+document.querySelector('.y').value+'<br>'+document.querySelector('.z').value;
+    const book=new createbook(document.querySelector('.x').value,document.querySelector('.y').value,document.querySelector('.z').value);
+    mylibrary.push(book);
     document.querySelector('.x').value='';document.querySelector('.y').value='';document.querySelector('.z').value='';
     let str=random();
     div.style.backgroundColor=str;
     div.addEventListener('click',function(){
        div.remove();
     });
-    container.appendChild(div); 
+    container.appendChild(div);
+   
 });
 
 
